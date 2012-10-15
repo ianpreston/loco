bdfcf8469229bb41c593601804c3f9fe1bfef6b4
Ian Preston <ian@ian-preston.com>
2012-10-15 00:07:23 -0700

    version bump to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='loco',
-      version='0.1',
+      version='0.2',
       description='Edit remote files locally',
       author='Ian Preston',
       author_email='ian@ian-preston.com',
